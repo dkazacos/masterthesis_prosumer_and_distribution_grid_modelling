3c89883dce263fbf18bea205ffa83e970f3c802e
dkazacos <dkazacos@stud.hs-offenburg.de>
2020-02-17 21:59:19 +0100

    chore: utils migrated from development to utils

--- v0.1/Prosumer.py
+++ v0.1/Prosumer.py
@@ -225,18 +225,6 @@ class PVgen(object):
         
         return self.rated_p*self.sys_loss # TODO! placeholder for PV model
 
-def timegrid(self):
-    """
-    Extracts time step (in seconds) from the time grid property of data.
-    Data must be pandas DataFrame or Series
-    """
-    if self.p_load.index.dtype == object:
-        self.p_load.index = pd.to_datetime(self.p_load.index)
-        return (self.p_load.index[1] - self.p_load.index[0]) // pd.Timedelta('1s')
-    
-    elif self.p_kw.index.dtype != object:
-        return (self.p_load.index[1] - self.p_load.index[0]) // pd.Timedelta('1s')
-
 class Prosumer(object):
     
     """
