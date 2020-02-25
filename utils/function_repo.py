a767f5a07521a0ffff9974eb28ca9db595e43d23
dkazacos <dkazacos@stud.hs-offenburg.de>
2020-02-25 11:54:17 +0100

    refactor: timegrid func importable

--- utils/function_repo.py
+++ utils/function_repo.py
@@ -7,14 +7,14 @@ Created on Mon Feb 17 21:55:22 2020
 
 import pandas as pd
 
-def timegrid(self):
+def timegrid(data):
     """
     Extracts time step (in seconds) from the time grid property of data.
     Data must be pandas DataFrame or Series
     """
-    if self.p_load.index.dtype == object:
-        self.p_load.index = pd.to_datetime(self.p_load.index)
-        return (self.p_load.index[1] - self.p_load.index[0]) // pd.Timedelta('1s')
+    if data.p_load.index.dtype == object:
+        data.p_load.index = pd.to_datetime(data.p_load.index)
+        return (data.p_load.index[1] - data.p_load.index[0]) // pd.Timedelta('1s')
     
-    elif self.p_kw.index.dtype != object:
-        return (self.p_load.index[1] - self.p_load.index[0]) // pd.Timedelta('1s')
+    elif data.p_kw.index.dtype != object:
+        return (data.p_load.index[1] - data.p_load.index[0]) // pd.Timedelta('1s')
