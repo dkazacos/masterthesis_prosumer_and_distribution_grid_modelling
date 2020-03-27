cbca94bfb3d779dbc4d66a8fd3de079641198f58
dkazacos <dkazacos@stud.hs-offenburg.de>
2020-03-27 12:07:44 +0100

    test: rearrange test environment example 1

--- net_sim_ex1.py
+++ net_sim_ex1.py
@@ -5,6 +5,7 @@ Created on Mon Mar  9 10:05:26 2020
 @author: Seta
 """
 
+from collections import defaultdict
 import os
 import sys
 import random
@@ -109,6 +110,18 @@ def neighborhood(net):
 
     return neighborhood
 
+# Initialize results storage
+def create_output_writer(net, time_steps, output_dir):
+    ow = ts.OutputWriter(net, time_steps, output_path=output_dir,
+                         output_file_type=".xls", log_variables=list())
+    # these variables are saved to the harddisk after / during the time series loop
+    ow.log_variable('res_ext_grid', 'p_mw')
+    # ow.log_variable('res_load', 'p_mw')
+    ow.log_variable('res_bus', 'vm_pu')
+    ow.log_variable('res_line', 'loading_percent')
+    # ow.log_variable('res_line', 'i_ka')
+    return ow
+
 # ============================================================================
 # RUN example
 # Load data
@@ -121,51 +134,30 @@ net = simple_net()
 nh = neighborhood(net)
 
 now=time.time()
-# Initialize results storage
-def create_output_writer(net, time_steps, output_dir):
-    ow = ts.OutputWriter(net, time_steps, output_path=output_dir,
-                         output_file_type=".xls", log_variables=list())
-    # these variables are saved to the harddisk after / during the time series loop
-    ow.log_variable('res_ext_grid', 'p_mw')
-    # ow.log_variable('res_load', 'p_mw')
-    ow.log_variable('res_bus', 'vm_pu')
-    ow.log_variable('res_line', 'loading_percent')
-    # ow.log_variable('res_line', 'i_ka')
-    return ow
-t           = []
-th_overload = pd.DataFrame()
-vm_pu_bus   = pd.DataFrame()
-slack_p     = pd.DataFrame()
+res = defaultdict(list)
 # Run stepwise simulation extracting load and irradiation
-for i, (ir, ld) in enumerate(zip(irr[1080:1140], load[1080:1140]*15)):
-    # print('new iteration', i)
-    print(ld)
+for i, (ir, ld) in enumerate(zip(irr[1080:1140], load[1080:1140]*10)):
     # Instantiate a controller unit for each Prosumer's load
     for j, (key, val) in enumerate(nh.items()):
         d = pd.DataFrame(index=[0])
         # Randomly generate a variation of a prosumer load within +/- 30 %
-        ft = random.randint(1,30)/1000
-        ld = ld*(1-ft)
-        # print(ld)
+        ld = ld*(1-random.randint(1,30)/1000)
         val.run_pflow(ir, ld, timestep, timestamp=irr[1080:1140].index[i])
         d[key] = -val.meta['p_grid_flow'][-1]/1000
-        # sources[key] = p_grid
         ds = ts.DFData(d)
-        print(d)
-        # ds = ts.DFData(df)
         control.ConstControl(net, element='load',
                              element_index=[j],
                              variable='p_mw',  data_source=ds,
                              profile_name=[key])
 
     # Run power flow calculation at every timestep iteration
-    # mid = time.time()
     ow = create_output_writer(net,[], "E:/Temp")
     ts.run_timeseries(net, verbose=False)
 
     # Store line overload, voltage at buses and slack power balance
-    t.append(irr.index[i])
-    th_overload = th_overload.append(net.res_line.loading_percent.transpose())
-    vm_pu_bus = vm_pu_bus.append(net.res_bus.vm_pu.transpose())
-    slack_p = slack_p.append(net.res_ext_grid.p_mw)
+    res['Time'].append(irr.index[i])
+    res['th_overload'].append(net.res_line.loading_percent.transpose())
+    res['vm_pu_bus'].append(net.res_bus.vm_pu.transpose())
+    res['slack_p'].append(net.res_ext_grid.p_mw)
     # print('Time since beginning of simulation: ', time.time() - now)
+results = pd.DataFrame(res)
